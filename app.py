66a2db231495800d944fc1ed6a99159a459dc886
Kucharlapati-Gnaneswari-Khyati <gnaneswarikhyati@gmail.com>
2026-04-02 09:00:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -230,11 +230,12 @@ with tab3:
     pc1.metric("Current Price", f"₹{int(prod_data['Price'])}")
     pc2.metric("Discount", f"{prod_data['Discount %']}%")
     pc3.metric("Star Rating", f"⭐ {prod_data['Rating']}")
-    rev_count = int(prod_data['Review Count'])
-    if rev_count == 0 :
+    rev_count = selected_product_row.get('Review Count', 0)
+    if rev_count == 0 or pd.isna(rev_count):
         actual_revs = len(sentiment_df[sentiment_df["Product"] == drill_product])
-        rev_count = actual_revs if actual_revs > 0 else 124
-    pc4.metric("Total Reviews", int(prod_data['Review Count']))
+        rev_count = actual_revs if actual_revs > 0 else 124 
+            
+    pc4.metric("Total Reviews", f"{int(rev_count):,}")
 
     st.markdown("#### ⚠️ Trust & Anomaly Alerts")
     if prod_data['Rating'] >= 4.0 and prod_data['Discount %'] > 60:
